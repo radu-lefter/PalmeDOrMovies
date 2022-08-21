7c9b4b69f82277afc49303548a9c1cc5ea0a83e9
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2022-08-21 16:00:21 +0100

    initial starter project commit

---
+++ src/main/java/com/qaproject/palmedor/PalmeDOrMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.qaproject.palmedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalmeDOrMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalmeDOrMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qaproject/palmedor/PalmeDOrMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qaproject.palmedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalmeDOrMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
