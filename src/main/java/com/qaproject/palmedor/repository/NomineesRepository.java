6d9a05e8a9e21d1cd1fcba3ec88c05e3dc1f30f3
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2022-08-21 17:37:06 +0100

    created nominees repository

---
+++ src/main/java/com/qaproject/palmedor/repository/NomineesRepository.java
@@ -0,0 +1,9 @@
+package com.qaproject.palmedor.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.qaproject.palmedor.model.Nominees;
+
+public interface NomineesRepository extends CrudRepository<Nominees, Integer>{
+
+}
